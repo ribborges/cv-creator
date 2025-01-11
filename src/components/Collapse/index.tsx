d07888dcc12bb2dc050ab5347ec4fd2c9369cbf3
Richard Borges <ribborges@outlook.com>
2025-01-11 16:23:31 -0300

    style: remove SCSS styles from Collapse component and implement TailwindCSS for styling

--- src/components/Collapse/index.tsx
+++ src/components/Collapse/index.tsx
@@ -1,6 +1,5 @@
 import { ReactNode, useState } from 'react';
-
-import './_style.scss';
+import { CaretRightFill } from 'react-bootstrap-icons';
 
 interface collapseProps {
     title: ReactNode,
@@ -8,10 +7,19 @@ interface collapseProps {
 }
 
 export default function Collapse(props: collapseProps) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    }
+
     return (
-        <details className="details">
-            <summary className="summary">{props.title}</summary>
-            <div className="content">
+        <details className="flex gap-5">
+            <summary onClick={handleClick} className="flex gap-5 items-center text-purple-700 hover:text-pink-700 transition duration-500 flex-2">
+                <CaretRightFill className={`transform ${isOpen ? 'rotate-90' : ''} transition duration-500`} />
+                {props.title}
+            </summary>
+            <div>
                 {props.children}
             </div>
         </details>
