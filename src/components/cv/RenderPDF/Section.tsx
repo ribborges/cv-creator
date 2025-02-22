46d9d7e9c555c54095a654870efaacb9d3ac1448
Richard Borges <ribborges@outlook.com>
2025-02-22 03:30:33 -0300

    fix: adjust border thickness in Section component title

--- src/components/cv/RenderPDF/Section.tsx
+++ src/components/cv/RenderPDF/Section.tsx
@@ -6,7 +6,7 @@ interface SectionProps {
 export default function Section(props: SectionProps) {
     return (
         <div className="flex flex-col gap-2 flex-1">
-            <span className="text-base font-bold border-b border-solid border-black">
+            <span className="text-base font-bold border-b-[1pt] border-solid border-black">
                 {props.title}
             </span>
             <div className="text-justify">
