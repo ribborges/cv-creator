770e0998d373575ebee1c45b6ce94ccf39298a39
Richard Borges <ribborges@outlook.com>
2025-02-25 14:54:24 -0300

    fix: set language value in Input component for proper language display

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
 import { Translate } from 'react-bootstrap-icons';
 import { useTranslation } from 'react-i18next';
-
 import CvForm from '../components/forms/CvForm';
 import Footer from '../components/Footer';
 import { Input } from '../components/input/Input';
@@ -18,7 +17,7 @@ export default function App() {
     <main id="no-print" className="text-zinc-950 dark:text-zinc-100 bg-white dark:bg-black app">
       <NavBar>
         <NavItemContainer>
-          <Input icon={<Translate />} type="option" onChange={handleChangeLanguage}>
+          <Input icon={<Translate />} type="option" onChange={handleChangeLanguage} value={i18n.language}>
             <option value="en-US">English</option>
             <option value="pt-BR">Português</option>
           </Input>
