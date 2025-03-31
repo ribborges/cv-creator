9374f3a30db11cf94c97e67c20c8e7a4d26d127c
Richard Borges <ribborges@outlook.com>
2025-03-31 18:18:23 -0300

    refactor: update Footer component to use Anchor for links and improve styling

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,17 +1,30 @@
+import Anchor from "../Anchor";
+
 export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="
-      flex items-center justify-center
-      bg-zinc-400 bg-opacity-50 dark:bg-zinc-800 dark:bg-opacity-50
-      border border-solid border-zinc-500 border-opacity-50 dark:border-zinc-700 dark:border-opacity-50
-      rounded-lg
-      p-10 m-5
-      hover:scale-95
-      transition duration-500
-    ">
-      <p>Developed by: <a href='https://www.linkedin.com/in/ribborges/' target={'_blank'}>Richard de Carvalho Borges</a> with <a href='https://vite.dev/' target={'_blank'}>Vite</a>, <a href="https://www.typescriptlang.org/" target={'_blank'}>TypeScript</a> and <a href='https://tailwindcss.com/' target={'_blank'}>TailwindCSS</a> | {currentYear}</p>
+    <footer
+      className="
+        text-xs
+        flex items-center justify-center
+        bg-zinc-200/50 dark:bg-zinc-900/50
+        border border-solid rounded-4xl border-zinc-300/50 dark:border-zinc-800/50
+        p-4 md:p-6 lg:p-8 m-4
+      "
+    >
+      <p>
+        {`Developed by: `}
+        <Anchor href='https://www.linkedin.com/in/ribborges/' target={'_blank'}>Richard de Carvalho Borges</Anchor>
+        {` with `}
+        <Anchor href='https://vite.dev/' target={'_blank'}>Vite</Anchor>
+        {`, `}
+        <Anchor href="https://www.typescriptlang.org/" target={'_blank'}>TypeScript</Anchor>
+        {` and `}
+        <Anchor href='https://tailwindcss.com/' target={'_blank'}>TailwindCSS</Anchor>
+        {` | `}
+        {currentYear}
+      </p>
     </footer>
   );
 }
