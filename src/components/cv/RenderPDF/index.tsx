ce2b1b9ad961d518fed64006b7bb041a10761349
Richard Borges <ribborges@outlook.com>
2025-04-02 00:26:39 -0300

    refactor: replace button with Button component in RenderPDF for consistency

--- src/components/cv/RenderPDF/index.tsx
+++ src/components/cv/RenderPDF/index.tsx
@@ -1,27 +1,19 @@
+import { Button } from "@/components/Input";
+
 import Print from "./Print";
 
 export default function RenderPDF() {
     return (
-        <>
-            <div className="flex justify-center p-2">
-                <button
-                    type="button"
-                    onClick={() => window.print()}
-                    className="
-                        p-2
-                        bg-zinc-300 dark:bg-zinc-700
-                        hover:bg-zinc-400 dark:hover:bg-zinc-600
-                        text-zinc-700 dark:text-zinc-300
-                        rounded-full
-                        transition duration-500
-                    "
-                >
-                    Download PDF
-                </button>
-            </div>
-            <div className="flex h-full md:w-5/6 lg:w-4/6 flex-col box-border p-12 bg-white text-black rounded-md overflow-scroll">
+        <div className="flex flex-col p-4 gap-4">
+            <Button
+                type="button"
+                onClick={() => window.print()}
+            >
+                Download PDF
+            </Button>
+            <div className="flex h-full flex-col box-border p-12 bg-white text-black rounded-md overflow-scroll">
                 <Print />
             </div>
-        </>
+        </div>
     );
 }
