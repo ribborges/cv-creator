cc721908025df8f08889b759ca167fa27d6d74de
Richard Borges <ribborges@outlook.com>
2025-06-27 11:11:53 -0300

    feat: replace language selection Input with Dropdown for improved UI

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,27 +1,38 @@
-import { Translate } from 'react-bootstrap-icons';
 import { useTranslation } from 'react-i18next';
 
 import CvForm from '@/components/forms/CvForm';
 import Footer from '@/components/Footer';
-import { Input } from '@/components/Input';
 import { NavBar, NavItemContainer } from '@/components/Navbar';
+import Dropdown from '@/components/Dropdown';
 
 export default function App() {
   const { i18n } = useTranslation();
-
-  function handleChangeLanguage(event: React.ChangeEvent<HTMLSelectElement>) {
-    const lang = event.target.value;
-    i18n.changeLanguage(lang);
-  }
 
   return (
     <main id="no-print" className="flex flex-col">
       <NavBar>
         <NavItemContainer>
-          <Input icon={<Translate />} type="select" onChange={handleChangeLanguage} value={i18n.language}>
-            <option value="en-US">English</option>
-            <option value="pt-BR">Português</option>
-          </Input>
+          <Dropdown align={'right'} items={[
+            {
+              type: 'button',
+              icon: <img src='https://img.icons8.com/color/48/brazil-circular.png' />,
+              label: 'Português',
+              onClick: () => i18n.changeLanguage('pt-BR'),
+            }, {
+              type: 'button',
+              icon: <img src='https://img.icons8.com/color/48/great-britain-circular.png' />,
+              label: 'English',
+              onClick: () => i18n.changeLanguage('en-US'),
+            }
+          ]}>
+            {
+              i18n.language === 'pt-BR' ? (
+                <img src='https://img.icons8.com/color/48/brazil-circular.png' />
+              ) : (
+                <img src='https://img.icons8.com/color/48/great-britain-circular.png' />
+              )
+            }
+          </Dropdown>
         </NavItemContainer>
       </NavBar>
 
