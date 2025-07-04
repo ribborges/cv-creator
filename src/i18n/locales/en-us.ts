66006d8437d4125c216061720079cfe8cd08c9da
Richard Borges <ribborges@outlook.com>
2025-07-04 16:19:29 -0300

    feat: add English and Portuguese translations for navigation and content sections

--- src/i18n/locales/en-us.ts
+++ src/i18n/locales/en-us.ts
@@ -2,8 +2,63 @@ import { t } from "i18next";
 
 export default {
     translations: {
+        nav: {
+            home: 'Home',
+            about: 'About',
+            source: 'Source'
+        },
+        homePage: {
+            title: 'Welcome to CV Creator',
+            subtitle: 'Create your CV with ease!',
+            description: {
+                1: 'CV Creator is an open-source project that allows you to create your CV in a simple and intuitive way.',
+                2: 'You can start from scratch or import your existing CV in JSON or PDF format.'
+            },
+            start: 'Get started',
+            import: 'Drag and drop the .json or .pdf file or click to upload'
+        },
+        aboutPage: {
+            title: 'About CV Creator',
+            description: 'CV Creator is an open-source project that allows you to create your CV in a simple and intuitive way.',
+            credits: 'Created by',
+            license: 'Licensed under the Mozilla Public License 2.0 (MPL-2.0).',
+            help: {
+                title: 'Need help?',
+                howTo: {
+                    title: 'How to use CV Creator?',
+                    description: {
+                        1: 'Fill in the form with your personal information, education, work experience, certifications, languages, skills, and projects.',
+                        2: 'Yor CV will be rendered in real-time as you fill in the form.',
+                        3: 'Once you complete the form, you can export your CV as a JSON file or view it as a PDF.'
+                    }
+                },
+                issue: {
+                    title: 'Found a bug or have a feature request?',
+                    description: 'You can report bugs or request features on the GitHub repository:',
+                },
+                contribute: {
+                    title: 'Want to contribute?',
+                    description: 'You can contribute to the project by opening a pull request on the GitHub repository:'
+                }
+            }
+        },
+        menu: {
+            info: 'Info',
+            education: 'Education',
+            experience: 'Experience',
+            certifications: 'Certifications',
+            languages: 'Languages',
+            skills: 'Skills',
+            projects: 'Projects',
+            print: 'Download .pdf',
+            export: 'Export .json',
+        },
         info: {
             title: 'Personal information',
+            description: {
+                1: 'This section contains your personal details such as name, contact information, and a brief summary of your professional background.',
+                2: 'Make sure to keep this information up-to-date to reflect your current status and contact details.'
+            },
             name: 'Name',
             personal_title: 'Title',
             phone: 'Phone',
@@ -14,6 +69,10 @@ export default {
         },
         education: {
             title: 'Education history',
+            description: {
+                1: 'This section allows you to add your educational background, including degrees, institutions, and dates attended.',
+                2: 'Make sure to include relevant details that highlight your academic achievements and qualifications.'
+            },
             name: 'School name',
             degree: 'Degree',
             field: 'Field of study',
@@ -24,6 +83,10 @@ export default {
         },
         experience: {
             title: 'Work experience',
+            description: {
+                1: 'This section allows you to add your work experience, including job titles, companies, and dates of employment.',
+                2: 'Make sure to include relevant details that highlight your professional achievements and responsibilities.'
+            },
             company: 'Company name',
             position: 'Job title',
             location: 'Location',
@@ -33,11 +96,19 @@ export default {
         },
         certifications: {
             title: 'Licenses & Certifications',
+            description: {
+                1: 'This section allows you to add your certifications, including the name of the certification and issuing organization.',
+                2: 'Make sure to include relevant details that highlight your professional qualifications and achievements.'
+            },
             name: 'License/Certification name',
             organization: 'Issuing organization'
         },
         languages: {
             title: 'Languages',
+            description: {
+                1: 'This section allows you to add the languages you speak, along with your proficiency level in each language.',
+                2: 'Make sure to include relevant details that highlight your language skills and abilities.'
+            },
             language: 'Language',
             select: 'Select',
             novice: 'Elementary',
@@ -48,12 +119,21 @@ export default {
         },
         skills: {
             title: 'Skills',
+            description: {
+                1: 'This section allows you to add your skills, categorized into hard and soft skills.',
+                2: 'Hard skills are specific, teachable abilities or knowledge sets, while soft skills are interpersonal skills that help you interact effectively with others.',
+                3: 'Make sure to include skills that are relevant to the job you are applying for, as well as any certifications or training you have received.'
+            },
             skill: 'Skill',
             hard: 'Hard skills',
             soft: 'Soft skills'
         },
         projects: {
             title: 'Projects',
+            description: {
+                1: 'This section allows you to add your projects, including the project name, description, and technologies used.',
+                2: 'Make sure to include relevant details that highlight your project management skills and technical expertise.'
+            },
             project: 'Project',
             name: 'Project name',
             skills: 'Skills used',
--- src/i18n/locales/pt-br.ts
+++ src/i18n/locales/pt-br.ts
@@ -2,8 +2,63 @@ import { t } from "i18next";
 
 export default {
     translations: {
+        nav: {
+            home: 'Início',
+            about: 'Sobre',
+            source: 'Código Fonte'
+        },
+        homePage: {
+            title: 'Bem-vindo ao Criador de Currículos',
+            subtitle: 'Crie seu currículo com facilidade!',
+            description: {
+                1: 'O Criador de Currículos é um projeto de código aberto que permite criar seu currículo de forma simples e intuitiva.',
+                2: 'Você pode começar do zero ou importar seu currículo existente nos formatos JSON ou PDF.'
+            },
+            start: 'Começar',
+            import: 'Arraste e solte o arquivo .json ou .pdf ou clique para fazer o upload'
+        },
+        aboutPage: {
+            title: 'Sobre o Criador de Currículos',
+            description: 'O Criador de Currículos é um projeto de código aberto que permite criar seu currículo de forma simples e intuitiva.',
+            credits: 'Criado por',
+            license: 'Licenciado sob a Licença Pública Mozilla 2.0 (MPL-2.0).',
+            help: {
+                title: 'Precisa de ajuda?',
+                howTo: {
+                    title: 'Como usar o Criador de Currículos?',
+                    description: {
+                        1: 'Preencha o formulário com suas informações pessoais, histórico acadêmico, experiência profissional, certificações, idiomas, habilidades e projetos.',
+                        2: 'Seu currículo será renderizado em tempo real à medida que você preenche o formulário.',
+                        3: 'Depois de completar o formulário, você pode exportar seu currículo como um arquivo JSON ou visualizá-lo como um PDF.'
+                    }
+                },
+                issue: {
+                    title: 'Encontrou um bug ou tem uma solicitação de recurso?',
+                    description: 'Você pode relatar bugs ou solicitar recursos no repositório do GitHub:'
+                },
+                contribute: {
+                    title: 'Quer contribuir?',
+                    description: 'Você pode contribuir para o projeto abrindo um pull request no repositório do GitHub:'
+                }
+            }
+        },
+        menu: {
+            info: 'Informações',
+            education: 'Educação',
+            experience: 'Experiência',
+            certifications: 'Certificações',
+            languages: 'Idiomas',
+            skills: 'Habilidades',
+            projects: 'Projetos',
+            print: 'Baixar .pdf',
+            export: 'Exportar .json'
+        },
         info: {
             title: 'Informações pessoais',
+            description: {
+                1: 'Esta seção contém seus dados pessoais, como nome, informações de contato e um breve resumo do seu histórico profissional.',
+                2: 'Certifique-se de manter essas informações atualizadas para refletir seu status e detalhes de contato atuais.'
+            },
             name: 'Nome',
             personal_title: 'Título',
             phone: 'Telefone',
@@ -13,6 +68,10 @@ export default {
         },
         education: {
             title: 'Histórico acadêmico',
+            description: {
+                1: 'Esta seção permite que você adicione seu histórico educacional, incluindo graus, instituições e datas de frequência.',
+                2: 'Certifique-se de incluir detalhes relevantes que destaquem suas conquistas acadêmicas e qualificações.'
+            },
             school: 'Nome da escola',
             degree: 'Grau',
             field: 'Área de estudo',
@@ -23,6 +82,10 @@ export default {
         },
         experience: {
             title: 'Experiência de trabalho',
+            description: {
+                1: 'Esta seção permite que você adicione sua experiência de trabalho, incluindo cargos, empresas e datas de emprego.',
+                2: 'Certifique-se de incluir detalhes relevantes que destaquem suas conquistas e responsabilidades profissionais.'
+            },
             company: 'Nome da empresa',
             position: 'Cargo',
             location: 'Localização',
@@ -32,11 +95,19 @@ export default {
         },
         certifications: {
             title: 'Licenças e Certificações',
+            description: {
+                1: 'Esta seção permite que você adicione suas certificações, incluindo o nome da certificação e a organização emissora.',
+                2: 'Certifique-se de incluir detalhes relevantes que destaquem suas qualificações e conquistas profissionais.'
+            },
             name: 'Nome da Licença/Certificação',
             organization: 'Organização emissora'
         },
         languages: {
             title: 'Idiomas',
+            description: {
+                1: 'Esta seção permite que você adicione os idiomas que fala, juntamente com seu nível de proficiência em cada idioma.',
+                2: 'Certifique-se de incluir detalhes relevantes que destaquem suas habilidades e capacidades linguísticas.'
+            },
             language: 'Idioma',
             select: 'Selecione',
             novice: 'Básico',
@@ -47,12 +118,21 @@ export default {
         },
         skills: {
             title: 'Habilidades',
+            description: {
+                1: 'Esta seção permite que você adicione suas habilidades, categorizadas em habilidades técnicas e interpessoais.',
+                2: 'Habilidades técnicas são habilidades específicas e ensináveis, enquanto habilidades interpessoais são habilidades sociais que ajudam você a interagir efetivamente com os outros.',
+                3: 'Certifique-se de incluir habilidades relevantes para o trabalho ao qual está se candidatando, bem como quaisquer certificações ou treinamentos que tenha recebido.'
+            },
             skill: 'Habilidade',
             hard: 'Habilidades técnicas',
             soft: 'Habilidades interpessoais'
         },
         projects: {
             title: 'Projetos',
+            description: {
+                1: 'Esta seção permite que você adicione seus projetos, incluindo o nome do projeto, descrição e tecnologias utilizadas.',
+                2: 'Certifique-se de incluir detalhes relevantes que destaquem suas habilidades de gerenciamento de projetos e expertise técnica.'
+            },
             project: 'Projeto',
             name: 'Nome do projeto',
             skills: 'Habilidades utilizadas',
