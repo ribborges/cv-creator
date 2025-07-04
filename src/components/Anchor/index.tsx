7b45db8229036e80b6dd9bbc2c97e3cfef1ecbd2
Richard Borges <ribborges@outlook.com>
2025-07-04 16:22:50 -0300

    fix: add cursor-pointer class to Anchor component for better UX

--- src/components/Anchor/index.tsx
+++ src/components/Anchor/index.tsx
@@ -1,6 +1,6 @@
 import { AnchorHTMLAttributes } from "react";
 
-export default function Anchor({ className = "text-purple-600 hover:text-pink-600 transition duration-500", ...props }: AnchorHTMLAttributes<HTMLAnchorElement>) {
+export default function Anchor({ className = "text-purple-600 hover:text-pink-600 transition duration-500 cursor-pointer", ...props }: AnchorHTMLAttributes<HTMLAnchorElement>) {
     return (
         <a
             className={className}
